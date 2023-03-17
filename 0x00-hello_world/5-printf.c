355b40b52919b9865c382e86b3e953f2bc8fa7c6
yohannes tesfu <yohannestesfu5@gmail.com>
2023-03-17 06:34:47 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
- * main - A program that prints a text followed by a new line using printf function
+ * main - A program that prints a text followed by a new line using
+ * printf function
  * return: 0
  */
-int main (void)
+int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n");
-return(0);
+printf("with proper grammar, but the outcome is a piece of art,\n")
+	return (0)
 }
